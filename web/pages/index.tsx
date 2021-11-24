d5c6595f4062acf2b069810e1940da3fff740c35
Dragan Okanovic <dragan.okan@gmail.com>
2021-11-24 08:25:24 +0100

    update home page

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -27,7 +27,7 @@ const Home: NextPage = () => {
             textTransform: "lowercase",
           }}
         >
-          Record &middot; Replay &middot; Debug
+          Capture &middot; Replay &middot; Debug
         </h3>
 
         <p
@@ -50,15 +50,16 @@ const Home: NextPage = () => {
 
         <h3>how it works?</h3>
 
-        <ol style={{
-          display: 'inline-block',
-          margin: '0, auto',
-          textAlign: 'left'
-        }}>
-          <li>inject recorder script</li>
-          <li>detect issue and pull out the session recordings</li>
-          <li>replay recording and patch your app live</li>
-          <li>push fixes!</li>
+        <ol
+          style={{
+            display: "inline-block",
+            margin: "0, auto",
+            textAlign: "left",
+          }}
+        >
+          <li>inject recorder script &amp; capture data</li>
+          <li>replay captured data &amp; debug</li>
+          <li>patch your app live &amp; push fixes</li>
         </ol>
       </div>
     </div>
